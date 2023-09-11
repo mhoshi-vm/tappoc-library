c31b0a19a10ea6b175e7e1e4e0bc234be7365022
machih <machih@vmware.com>
2023-09-11 23:37:58 +0900

    Apply sb fix

---
+++ src/main/java/jp/co/normalcompany/library/observability/WfBindings.java
@@ -0,0 +1,60 @@
+package jp.co.normalcompany.library.observability;
+
+import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.cloud.bindings.Binding;
+import org.springframework.cloud.bindings.Bindings;
+import org.springframework.cloud.bindings.boot.BindingsPropertiesProcessor;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
+import java.util.Map;
+
+public class WfBindings implements BindingsPropertiesProcessor {
+
+    public static final String TYPE = "wavefront";
+
+    @Override
+    public void process(Environment environment, Bindings bindings, Map<String, Object> map) {
+
+        List<Binding> wfBindings = bindings.filterBindings(TYPE);
+        if (wfBindings.isEmpty()) {
+            map.put("management.endpoint.wavefront.enabled", "false");
+            map.put("management.wavefront.metrics.export.enabled", "false");
+            map.put("management.wavefront.api-token", "dummy");
+            map.put("management.tracing.enabled", "false");
+        }
+        else {
+            map.put("management.endpoint.wavefront.enabled", "true");
+            map.put("management.wavefront.metrics.export.enabled", "true");
+            map.put("management.tracing.enabled", "true");
+            map.put("management.tracing.sampling.probability", "1.0");
+        }
+        bindings.filterBindings(TYPE).forEach(binding -> {
+            var mapper = new WfBindings.BindingPropertiesMapper(binding.getSecret(), map);
+            mapper.map("api-token", "management.wavefront.api-token");
+            mapper.map("uri", "management.wavefront.uri");
+
+        });
+    }
+
+    static class BindingPropertiesMapper {
+
+        private final Map<String, String> secret;
+
+        private final Map<String, Object> properties;
+
+        private final PropertyMapper mapper;
+
+        public BindingPropertiesMapper(Map<String, String> secret, Map<String, Object> properties) {
+            this.secret = secret;
+            this.properties = properties;
+            this.mapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
+        }
+
+        public void map(String from, String to) {
+            mapper.from(secret.get(from)).to(value -> this.properties.put(to, value));
+        }
+
+    }
+
+}
